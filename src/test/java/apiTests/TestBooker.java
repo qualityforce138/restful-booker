f4d5ec0d13924c2dd6d37da67c3aec96e303c828
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-07-15 10:57:47 -0300

    Criação testGetBooking concluido

--- src/test/java/apiTests/TestBooker.java
+++ src/test/java/apiTests/TestBooker.java
@@ -4,7 +4,12 @@ import io.cucumber.java.en.Given;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 public class TestBooker {
     //teste commit leonardo.
@@ -12,28 +17,37 @@ public class TestBooker {
     //teste chicão comit
     //teste commit Humberto
 
-String baseURL = "https://restful-booker.herokuapp.com/booking";
+String uri = "https://restful-booker.herokuapp.com/booking";
 String ct = "application/json";
+public static String buscarArquivoJson(String arquivoJson) throws IOException {
+
+    return new String(Files.readAllBytes(Paths.get(arquivoJson)));
+    }
 
     @Test
     @Order(1)
-    public void testGetBooking(){
-        // Dados de Entrega
-
-
+    public void testGetBooking() throws IOException {
+        // Dados de Entrada
+        String jsonBody = buscarArquivoJson("src/test/resources/json/book.json");
+        // Para realizar o teste verificar verificar inicialmente os dados da lista
         // Configura
         given()
-                .log().all()
                 .contentType(ct)
-                .body()
-        .when()
-
+                .log().all()
+                .body(jsonBody)
         // Executa
-
+        .when()
+                .get(uri)
 
         // Valida
-
-
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("bookingid [0]",is(13))
+                .body("bookingid [1]",is(9))
+                .body("bookingid [2]",is(48))
+                .body("bookingid [4]",is(32))
+        ;
 
     }
 
