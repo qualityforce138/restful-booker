b983ee53dcce376a54cca365ed13d00e8b9e4394
Everton <evertonulisystem@gmail.com>
2023-07-14 21:23:45 -0300

    Definindo variaveis

--- src/test/java/apiTests/TestBooker.java
+++ src/test/java/apiTests/TestBooker.java
@@ -1,8 +1,22 @@
 package apiTests;
 
-public class TestBooker {
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 
+public class TestBooker {
     //teste commit leonardo.
     //teste commit Everton.
+
+String uri = "https://restful-booker.herokuapp.com/booking";
+String ct = "application/json";
+
+    @Test
+    @Order(4)
+    public void testDeleteBooking(){
+
+
+    }
+
+
 
 }
